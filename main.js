02f07b26326c2bd084d9452415ac01640aa721c1
Abhishek Sharma <abhishek859573@gmail.com>
2023-11-26 11:50:59 +0530

    publisting test

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const profile = {
-    name: "shardakhapra",
-    post: 195,
+    
     followers: 5690000,
     following: 4,
 };
 
-console.log(Abhishek);
+console.log(profile);
 console.log(typeof profile);
